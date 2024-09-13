8088c8fd8a75c99008ddfb8f4df09c34676c1de6
Frowmza <66181451+Frowmza@users.noreply.github.com>
2024-09-13 18:08:32 +0100

    feat: take house objects / exit house

--- server/init.lua
+++ server/init.lua
@@ -15,13 +15,14 @@ end
 
 function House:playerEnter(source, initial)
     local id = self.id
-    self.private.insidePlayers[source] = true
+    local private = self.private
+    private.insidePlayers[source] = true
     SetPlayerRoutingBucket(source, id)
     Player(source).state.instance = id
 
     TriggerClientEvent("bl_houserobbery:client:enterHouse", source, {
         id = id,
-        interiorName = self.private.interior,
+        interiorName = private.interior,
         blackOut = self.blackOut
     })
 
@@ -33,6 +34,45 @@ end
 function House:isPlayerInside(source)
     return self.private.insidePlayers[source]
 end
+
+function House:takeObject(source, objectIndex)
+    local private = self.private
+    local object = require 'data.interiors'[private.interior].objects[objectIndex]
+    if not object then return end
+
+    local player = Framework.core.GetPlayer(source)
+    if not player then return end
+
+    player.addItem(object.item, 1)
+
+    for src in pairs(private.insidePlayers) do
+        TriggerClientEvent('bl_houserobbery:client:removeObject', src, objectIndex)
+    end
+end
+
+function House:playerExit(source)
+    local private = self.private
+    if not private.insidePlayers[source] then return end
+    private.insidePlayers[source] = nil
+    SetPlayerRoutingBucket(source, 1)
+    Player(source).state.instance = 1
+end
+
+RegisterNetEvent('bl_houserobbery:server:exitHouse', function(houseid)
+    local src = source
+    local activeHouse = activeHouses[houseid]
+    if not activeHouse or not activeHouse:isPlayerInside(src) then return end
+
+    activeHouse:playerExit(src)
+end)
+
+RegisterNetEvent('bl_houserobbery:server:takeObject', function(data)
+    local src = source
+    local activeHouse = activeHouses[data.id]
+    if not activeHouse or not activeHouse:isPlayerInside(src) then return end
+
+    activeHouse:takeObject(src, data.objectIndex)
+end)
 
 RegisterNetEvent('bl_houserobbery:server:enterHouse', function(houseid)
     local src = source
@@ -52,7 +92,6 @@ RegisterNetEvent('bl_houserobbery:server:updateHouse', function(data)
     local src = source
     local type, id = data.type, data.id
     local activeHouse = activeHouses[id]
-
     if not activeHouse or not activeHouse:isPlayerInside(src) then return end
 
     activeHouse[type] = true
