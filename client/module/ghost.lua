95a3e8ad98d2665c06873e87156d0a5646aafa3b
Frowmza <66181451+Frowmza@users.noreply.github.com>
2024-09-18 14:08:34 +0100

    feat: add ghost

---
+++ client/module/ghost.lua
@@ -0,0 +1,28 @@
+local store = require 'client.module.store'
+local currentHouse = store.currentHouse
+
+return function(coords)
+    local ghostData = require 'data.ghosts'
+
+    local model = lib.requestModel(ghostData.models[math.random(#ghostData.models)])
+    local ghost = CreateObject(model, coords.x, coords.y, coords.z - 0.85, false, false, false)
+    currentHouse.objects[#currentHouse.objects + 1] = ghost
+    SetEntityCollision(ghost, false, false)
+    SetEntityHeading(ghost, coords.w-180.0)
+
+    local anim = ghostData.anim
+    lib.requestAnimDict(anim.dict)
+    PlayEntityAnim(ghost, anim.name, anim.dict, 1000.0, true, true, true, 0, 136704)
+    RemoveAnimDict(anim.dict)
+
+    local ptfx = ghostData.ptfx
+    lib.requestNamedPtfxAsset(ptfx.name)
+    UseParticleFxAsset(ptfx.name)
+    local ptfxId = StartParticleFxLoopedOnEntity(ptfx.effect, ghost, 0.0, 0.0, 0.7, 0.0, 0.0, 0.0, 1.0, false, false, false)
+
+    currentHouse.ptfx[#currentHouse.ptfx + 1] = ptfxId
+    SetParticleFxLoopedEvolution(ptfxId, 'smoke', 10.0, true)
+    RemoveNamedPtfxAsset(ptfx.name)
+
+    SetModelAsNoLongerNeeded(model)
+end
---
+++ data/ghosts.lua
@@ -0,0 +1,17 @@
+return {
+    anim = {
+        dict = 'ANIM@SCRIPTED@FREEMODE@IG2_GHOST@',
+        name = 'float_1'
+    },
+    ptfx = {
+        name = 'scr_srr_hal',
+        effect = 'scr_srr_hal_ghost_haze'
+    },
+    models = {
+        `m23_1_prop_m31_ghostsalton_01a`,
+        `m23_1_prop_m31_ghostskidrow_01a`,
+        `m23_1_prop_m31_ghostzombie_01a`,
+        `m23_1_prop_m31_ghostrurmeth_01a`,
+        `m23_1_prop_m31_ghostjohnny_01a`,
+    }
+}
